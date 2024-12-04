1f7f75fc45a59cd3f550dafb608065a29270d80c
saguaraz24 <sebastianadresguaraz@gmail.com>
2024-12-04 10:23:04 -0300

    p
    p

--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/heroes', obtenerTodosLosSuperheroesController);
 router.get('/heroes/:id', obtenerSuperheroePorIdController);
 router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroePorAtributoController);
-router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
+router.get('/heroes/buscar/mayores-30', obtenerSuperheroesMayoresDe30Controller);
 
 
 
